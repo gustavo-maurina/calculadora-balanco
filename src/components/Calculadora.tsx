3db2751803956dd595b6fae19863bc1cc6f5e45e
Gustavo Farias <gustavocel82@gmail.com>
2022-03-16 11:28:55 -0300

    operacoes com interacao na interface

--- src/components/Calculadora.tsx
+++ src/components/Calculadora.tsx
@@ -1,13 +1,29 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 export function Calculadora() {
+  const [operacaoAtual, setOperacaoAtual] = useState<string>("");
+  const [resultado, setResutaldo] = useState<number>();
+
+  const createOperacaoAtual = (numero: string) => {
+    setOperacaoAtual((curValue) => curValue + numero);
+  };
+
+  const calculate = () => {
+    setResutaldo(eval(operacaoAtual));
+  };
+
   const createKeypad = (): ReactNode => {
     return (
       <div className="grid grid-cols-3 gap-4 w-full">
         {Array(9)
           .fill("x")
           .map((item: string, index: number) => (
-            <div className="btn-calculadora">{index + 1}</div>
+            <div
+              className="btn-calculadora"
+              onClick={() => createOperacaoAtual((index + 1).toString())}
+            >
+              {index + 1}
+            </div>
           ))}
       </div>
     );
@@ -16,14 +32,24 @@ export function Calculadora() {
   return (
     <div
       id="calculadora"
-      className="w-[300px] h-[380px] bg-gray-500 p-6 flex flex-col align-center gap-8 shadow-2xl rounded"
+      className="w-[300px] h-[420px] bg-gray-500 p-6 flex flex-col align-center gap-8 shadow-2xl rounded"
     >
-      <div id="visor" className="bg-green-900 w-full h-[100px] relative">
-        <div
-          id="valores-visor"
-          className="text-gray-300 absolute bottom-2 right-2 text-2xl"
-        >
-          5123
+      <div
+        id="visor"
+        className="bg-green-900 w-full h-[70px] relative flex flex-col"
+      >
+        <div className="absolute bottom-1 right-2">
+          <div id="valores-visor" className="text-gray-300 text-lg">
+            {operacaoAtual}
+          </div>
+          {resultado && (
+            <div
+              id="valores-visor"
+              className="text-gray-300 text-2xl text-right"
+            >
+              {resultado}
+            </div>
+          )}
         </div>
       </div>
       <div id="keypad" className="flex justify-between">
@@ -31,17 +57,40 @@ export function Calculadora() {
           {createKeypad()}
           <div id="last-line" className="flex pt-4 gap-4">
             <div className="btn-calculadora">0</div>
-            <div className="btn-calculadora w-full bg-orange-500 hover:bg-orange-400">
+            <div
+              className="btn-calculadora w-full bg-orange-500 hover:bg-orange-400"
+              onClick={calculate}
+            >
               =
             </div>
           </div>
         </div>
 
         <div id="operations" className="flex flex-col gap-4">
-          <div className="btn-calculadora">+</div>
-          <div className="btn-calculadora">-</div>
-          <div className="btn-calculadora pt-2">*</div>
-          <div className="btn-calculadora">/</div>
+          <div
+            className="btn-calculadora"
+            onClick={() => createOperacaoAtual(" + ")}
+          >
+            +
+          </div>
+          <div
+            className="btn-calculadora"
+            onClick={() => createOperacaoAtual(" - ")}
+          >
+            -
+          </div>
+          <div
+            className="btn-calculadora pt-2"
+            onClick={() => createOperacaoAtual(" * ")}
+          >
+            *
+          </div>
+          <div
+            className="btn-calculadora"
+            onClick={() => createOperacaoAtual(" / ")}
+          >
+            /
+          </div>
         </div>
       </div>
     </div>
