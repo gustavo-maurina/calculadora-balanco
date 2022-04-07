45cb33af7af19b276abb3a9284a686f42a74c542
Gustavo Farias <gustavocel82@gmail.com>
2022-04-07 16:46:43 -0300

    texto informativo

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,16 @@ import { CalculadoraProvider } from "./contexts/CalculadoraProvider";
 
 function App() {
   return (
-    <div className="bg-gray-700 flex flex-col justify-center items-center h-full min-h-screen gap-16 sm:flex-row">
+    <div className="bg-gray-700 flex flex-col justify-center items-center h-full min-h-screen gap-16">
+      <p className="text-slate-200 text-2xl mt-12 lg:mt-0">
+        Digite ou use a calculadora com o cursor !
+      </p>
       <CalculadoraProvider>
-        <KeyboardListener />
-        <Calculadora />
-        <HistoricoCalculos />
+        <div className="flex flex-col lg:flex-row gap-16">
+          <KeyboardListener />
+          <Calculadora />
+          <HistoricoCalculos />
+        </div>
       </CalculadoraProvider>
     </div>
   );
